663fe489676bfff26b3f2fa1e1c39abdb6922e7e
SeungHwan Kim <tmdghks9574@gmail.com>
2019-10-14 18:11:28 +0900

    gameSwHw

---
+++ gameSW_hw1/Assets/Scrit/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject health;
+    private int k = 0;
+    private void OnCollisionEnter(Collision other)
+    {
+        Destroy(other.gameObject);
+        Destroy(health.transform.GetChild(0).gameObject);
+        if (health.transform.childCount == 1)
+        {
+            Time.timeScale = 0;
+        }
+    }
+    private static float moveSpeed = 5;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x >= -8.5f)
+        {
+            transform.position += Vector3.left * Time.deltaTime * moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x <= 8.5f)
+        {
+            transform.position += Vector3.right * Time.deltaTime * moveSpeed;
+        }
+    }
+}
---
+++ gameSW_hw1/Assets/Scrit/SwordCreator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordCreator : MonoBehaviour
+{
+    public GameObject sword;
+
+    public float startTime = 1f;
+
+    public float repeatRate = 0.3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("createSword", startTime, repeatRate);
+    }
+
+    private void createSword()
+    {
+        var randomX = Random.Range(-8.5f, 8.5f);
+        var randomPosition = new Vector3(randomX, 5f, 0f);
+
+        GameObject cloneSword = Instantiate(sword);
+        cloneSword.transform.position = randomPosition;
+        cloneSword.gameObject.SetActive(true);
+    }
+}
